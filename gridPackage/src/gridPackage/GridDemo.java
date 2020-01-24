281254152a504f93a6b58bb8e066320d73f50179
SaiKrishna540 <krishnasai017@gmail.com>
2020-01-24 12:05:45 +0530

    Grid Package

---
+++ gridPackage/src/gridPackage/GridDemo.java
@@ -0,0 +1,39 @@
+package gridPackage;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class GridDemo {
+
+	public WebDriver driver;
+	
+	@Test
+	public void verifylogin() throws MalformedURLException {
+		DesiredCapabilities capabilities=DesiredCapabilities.chrome();
+		capabilities.setBrowserName("chrome");
+		capabilities.setPlatform(Platform.WINDOWS);
+		
+		driver=new RemoteWebDriver(new URL("http://172.23.102.8:4455/wd/hub"),capabilities);
+		
+		driver.get("http://demowebshop.tricentis.com/login");
+		String title=driver.getTitle();
+		System.out.println(title);
+		
+		driver.manage().window().maximize();
+		driver.findElement(By.id("Email")).sendKeys("rekhabr1@gmail.com");
+		driver.findElement(By.name("Password")).sendKeys("rekhab");
+		
+		driver.findElement(By.xpath("//input[@class='button-1 login-button']")).click();
+		System.out.println("Successfully Logged in");
+		driver.findElement(By.xpath("//a[@class='ico-logout']")).click();
+		driver.close();
+	}
+	
+}
